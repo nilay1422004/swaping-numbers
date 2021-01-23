e8c42e1d91b021549831daf7d9b34ed7b181aff9
nilay1422004 <77875216+nilay1422004@users.noreply.github.com>
2021-01-23 13:42:12 +0530

    code for swaping variable

---
+++ swaping number.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{ int a;
+int b;
+int temp;
+  printf("enter a and b value:\n");
+  scanf("%d%d",&a,&b);
+a=temp;
+a=b;
+b=temp;
+printf("value of a=%d and b=%d",a,b);
+}
+
